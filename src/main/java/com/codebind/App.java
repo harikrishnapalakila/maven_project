edc12e92569e8ecf250a88ab8f5cb961508ced10
harikrishna12334 <36328585+harikrishna12334@users.noreply.github.com>
2020-03-22 17:24:03 +0530

    code change
    code change

--- src/main/java/com/codebind/App.java
+++ src/main/java/com/codebind/App.java
@@ -2,7 +2,9 @@ package com.codebind;
 
 public class App {
        public String Sample () {
-    	   
-    	  return "sample";
+    	   {
+        System.out.println( "Hello 'This is harikrihna'!" );
+    }
+    	 
        }
 }
